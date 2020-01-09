cc7f557cc653ddc3ddfc36e64544863eb4df0707
Sinead913 <57669285+Sinead913@users.noreply.github.com>
2020-01-09 23:55:49 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+adverts = [
+    {
+        "advertid": 1,
+        "keyword": 'network',
+        "advert": "Network Engineer Job available in Belfast!"
+    },
+    {
+        "advertid": 2,
+        "keyword": 'network',
+        "advert": "These are the best networks in the world!"
+    },
+    {
+        "advertid": 3,
+        "keyword": 'test',
+        "advert": "Testing role available in Belfast IT company!"
+    }
+]
+
+class Advert(Resource):
+    def get(self, name):
+        for advert in adverts:
+            if(name == advert["keyword"]):
+                return advert, 200
+        return "User not found", 404
+
+api.add_resource(Advert, "/advert/<string:name>")
+
+app.run(debug=True)
