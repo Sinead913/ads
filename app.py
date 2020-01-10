f69719a4819eb93f3476a6a22b48a4e817e3da4b
Sinead913 <57669285+Sinead913@users.noreply.github.com>
2020-01-10 11:18:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,14 @@ adverts = [
 
 @app.route('/')
 def get_all_ads():
-    return jsonify(adverts)
+    # return jsonify(adverts)
+    return 'HELLO'
 
 @app.route('/adverts/<string:key>', methods=['GET'])
 def get_ads(key):
     for advert in adverts:
         if(key == advert["keyword"]):
-            return advert, 200
+            return key
     return "User not found", 404
 
 if __name__ == '__main__':
